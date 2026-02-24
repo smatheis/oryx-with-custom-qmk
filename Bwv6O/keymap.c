668237bb10b9d93f318d0908e20953aa4d848140
Steffen Matheis <s.matheis@technikum-kamenz.de>
2026-02-24 11:36:44 +0100

    Added custom key processing for HOME, END (linux), and for tab and app switching

--- Bwv6O/keymap.c
+++ Bwv6O/keymap.c
@@ -14,7 +14,13 @@ enum custom_keycodes {
   NAVIGATOR_INC_CPI,
   NAVIGATOR_DEC_CPI,
   NAVIGATOR_TURBO,
-  NAVIGATOR_AIM
+  NAVIGATOR_AIM,
+  SET_MAC,
+  SET_LINUX,
+  GUI_TAB,
+  S_GUI_TAB,
+  CTL_TAB,
+  S_CTL_TAB
 };
 
 
@@ -28,46 +34,46 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_voyager(
-    DUAL_FUNC_0,    DM_PLY1,        DM_PLY2,        LGUI(LSFT(KC_Z)),LGUI(DE_Z),     LGUI(KC_SPACE),                                 LALT(LGUI(LCTL(LSFT(KC_A)))),LALT(LGUI(LCTL(LSFT(KC_B)))),LALT(LGUI(LCTL(LSFT(KC_C)))),LALT(LGUI(LCTL(LSFT(KC_D)))),LALT(LGUI(LCTL(LSFT(KC_E)))),LALT(LGUI(LCTL(LSFT(KC_F)))),
-    DUAL_FUNC_1,    MT(MOD_LGUI, KC_TAB),MT(MOD_LSFT, KC_F),MT(MOD_LALT, KC_U),MT(MOD_LCTL, KC_M),KC_V,                                           KC_Q,           MT(MOD_RCTL, KC_L),MT(MOD_RALT, KC_B),MT(MOD_RSFT, KC_DOT),MT(MOD_RGUI, DE_Z),KC_ENTER,       
-    KC_ESCAPE,      MT(MOD_LGUI, KC_A),MT(MOD_LSFT, KC_C),MT(MOD_LALT, KC_I),MT(MOD_LCTL, KC_T),KC_D,                                           KC_R,           MT(MOD_RCTL, KC_N),MT(MOD_RALT, KC_H),MT(MOD_RSFT, KC_O),MT(MOD_RGUI, KC_S),QK_REP, 
+    DUAL_FUNC_0,    DM_PLY1,        DM_PLY2,        LGUI(LSFT(KC_Z)),LGUI(DE_Z),    LGUI(KC_SPACE),                                 LALT(LGUI(LCTL(LSFT(KC_A)))),LALT(LGUI(LCTL(LSFT(KC_B)))),LALT(LGUI(LCTL(LSFT(KC_C)))),LALT(LGUI(LCTL(LSFT(KC_D)))),LALT(LGUI(LCTL(LSFT(KC_E)))),LALT(LGUI(LCTL(LSFT(KC_F)))),
+    DUAL_FUNC_1,    MT(MOD_LGUI, KC_TAB),MT(MOD_LSFT, KC_F),MT(MOD_LALT, KC_U),MT(MOD_LCTL, KC_M),KC_V,                             KC_Q,           MT(MOD_RCTL, KC_L),MT(MOD_RALT, KC_B),MT(MOD_RSFT, KC_DOT),MT(MOD_RGUI, DE_Z),KC_ENTER,       
+    KC_ESCAPE,      MT(MOD_LGUI, KC_A),MT(MOD_LSFT, KC_C),MT(MOD_LALT, KC_I),MT(MOD_LCTL, KC_T),KC_D,                               KC_R,           MT(MOD_RCTL, KC_N),MT(MOD_RALT, KC_H),MT(MOD_RSFT, KC_O),MT(MOD_RGUI, KC_S),QK_REP, 
     SH_MON,         MO(2),          LT(5, KC_P),    DE_Y,           DUAL_FUNC_2,    KC_J,                                           KC_X,           KC_W,           KC_K,           KC_COMMA,       OSM(MOD_LSFT),  QK_AREP, 
-                                                    MO(1),          LT(4, KC_E),                                    LT(4, KC_SPACE),MO(2)
+                                                                    MO(1),          LT(4, KC_E),                                    LT(4, KC_SPACE),MO(2)
   ),
   [1] = LAYOUT_voyager(
     KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_EQUAL,                                       KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          
     RALT(KC_E),     LALT(KC_L),     LSFT(KC_SLASH), LALT(KC_5),     LALT(KC_6),     KC_CIRC,                                        DE_EXLM,        KC_NUBS,        LSFT(KC_NUBS),  DE_EQL,         DE_AMPR,        KC_TRANSPARENT, 
-    KC_NONUS_HASH,  LALT(LSFT(KC_7)),LSFT(KC_7),     LALT(KC_8),     LALT(KC_9),     LSFT(KC_RBRC),                                  DE_QST,         DE_LPRN,        DE_RPRN,        DE_MINS,        DE_COLN,        KC_TRANSPARENT, 
+    KC_NONUS_HASH,  LALT(LSFT(KC_7)),LSFT(KC_7),    LALT(KC_8),     LALT(KC_9),     LSFT(KC_RBRC),                                  DE_QST,         DE_LPRN,        DE_RPRN,        DE_MINS,        DE_COLN,        KC_TRANSPARENT, 
     KC_TRANSPARENT, MO(3),          DE_DLR,         LALT(KC_7),     LALT(KC_N),     LSFT(KC_EQUAL),                                 DE_PLUS,        DE_PERC,        DE_DQOT,        LSFT(KC_NONUS_HASH),DE_SCLN,        KC_TRANSPARENT, 
-                                                    KC_TRANSPARENT, LT(4, KC_SPACE),                                KC_TRANSPARENT, MO(3)
+                                                                    KC_TRANSPARENT, LT(4, KC_SPACE),                                KC_TRANSPARENT, MO(3)
   ),
   [2] = LAYOUT_voyager(
     KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_X,           KC_KP_PLUS,     KC_KP_MINUS,    KC_KP_ASTERISK, KC_KP_SLASH,    KC_NO,          
     KC_NO,          KC_PAGE_UP,     KC_BSPC,        KC_UP,          KC_DELETE,      KC_PGDN,                                        KC_BSPC,        KC_1,           KC_2,           KC_3,           KC_DOT,         KC_TRANSPARENT, 
     LGUI(KC_A),     LGUI(KC_LEFT),  KC_LEFT,        KC_DOWN,        KC_RIGHT,       LGUI(KC_RIGHT),                                 KC_TAB,         KC_4,           KC_5,           KC_6,           KC_0,           KC_COMMA,       
     KC_TRANSPARENT, KC_TRANSPARENT, LGUI(KC_X),     LGUI(KC_C),     LGUI(KC_V),     KC_ENTER,                                       DE_COLN,        KC_7,           KC_8,           KC_9,           DE_SCLN,        KC_NO,          
-                                                    MO(3),          LT(4, KC_SPACE),                                KC_TRANSPARENT, KC_TRANSPARENT
+                                                                    MO(3),          LT(4, KC_SPACE),                                KC_TRANSPARENT, KC_TRANSPARENT
   ),
   [3] = LAYOUT_voyager(
-    KC_TRANSPARENT, RGB_TOG,        TOGGLE_LAYER_COLOR,RGB_MODE_FORWARD,RGB_VAD,        RGB_VAI,                                        RGB_SPI,        RGB_SPD,        RGB_HUI,        RGB_HUD,        RGB_SAI,        RGB_SAD,        
-    KC_TRANSPARENT, KC_BRIGHTNESS_UP,KC_MEDIA_NEXT_TRACK,KC_TRANSPARENT, KC_TRANSPARENT, KC_AUDIO_VOL_UP,                                KC_PSCR,        KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_TRANSPARENT, 
-    KC_TRANSPARENT, KC_BRIGHTNESS_DOWN,KC_MEDIA_PLAY_PAUSE,KC_TRANSPARENT, KC_TRANSPARENT, KC_AUDIO_MUTE,                                  KC_SCRL,        KC_F5,          KC_F6,          KC_F7,          KC_F8,          KC_TRANSPARENT, 
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_MEDIA_PREV_TRACK,KC_NO,          KC_NO,          KC_AUDIO_VOL_DOWN,                                KC_NUM,         KC_F9,          KC_F10,         KC_F11,         KC_F12,         KC_TRANSPARENT, 
-                                                    KC_TRANSPARENT, KC_MS_JIGGLER_TOGGLE,                                KC_MS_JIGGLER_TOGGLE,KC_TRANSPARENT
+    SET_MAC,        RGB_TOG,        TOGGLE_LAYER_COLOR,RGB_MODE_FORWARD,RGB_VAD,    RGB_VAI,                                        RGB_SPI,        RGB_SPD,        RGB_HUI,        RGB_HUD,        RGB_SAI,        RGB_SAD,        
+    SET_LINUX,      KC_BRIGHTNESS_UP,KC_MEDIA_NEXT_TRACK,S_GUI_TAB, GUI_TAB,        KC_AUDIO_VOL_UP,                                KC_PSCR,        KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_TRANSPARENT, 
+    KC_TRANSPARENT, KC_BRIGHTNESS_DOWN,KC_MEDIA_PLAY_PAUSE,S_CTL_TAB,CTL_TAB,       KC_AUDIO_MUTE,                                  KC_SCRL,        KC_F5,          KC_F6,          KC_F7,          KC_F8,          KC_TRANSPARENT, 
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_MEDIA_PREV_TRACK,KC_NO,      KC_NO,          KC_AUDIO_VOL_DOWN,                              KC_NUM,         KC_F9,          KC_F10,         KC_F11,         KC_F12,         KC_TRANSPARENT, 
+                                                                    KC_TRANSPARENT, KC_MS_JIGGLER_TOGGLE,                           KC_MS_JIGGLER_TOGGLE,KC_TRANSPARENT
   ),
   [4] = LAYOUT_voyager(
     KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          
-    KC_NO,          KC_NO,          KC_NO,          LALT(LGUI(KC_UP)),LALT(LCTL(LSFT(KC_1))),KC_NO,                                          LALT(LCTL(LSFT(KC_A))),LALT(LCTL(LSFT(KC_B))),LALT(LCTL(LSFT(KC_C))),LALT(LCTL(LSFT(KC_D))),LALT(LCTL(LSFT(KC_E))),LALT(LCTL(LSFT(KC_F))),
-    LCTL(KC_SCLN),  KC_NO,          KC_NO,          LALT(LGUI(KC_DOWN)),LALT(LCTL(LSFT(KC_0))),ST_MACRO_0,                                     LALT(LCTL(LSFT(KC_G))),LALT(LCTL(LSFT(KC_H))),LALT(LCTL(LSFT(KC_I))),LALT(LCTL(LSFT(KC_J))),LALT(LCTL(LSFT(KC_K))),LALT(LCTL(LSFT(KC_Q))),
-    KC_TRANSPARENT, DUAL_FUNC_3,    DUAL_FUNC_4,    DUAL_FUNC_5,    LGUI(KC_S),     LGUI(LSFT(KC_I)),                                LALT(LCTL(LSFT(KC_L))),LALT(LCTL(LSFT(KC_M))),LALT(LCTL(LSFT(KC_N))),LALT(LCTL(LSFT(KC_O))),LALT(LCTL(LSFT(KC_P))),LALT(LCTL(LSFT(KC_R))),
-                                                    KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT
+    KC_NO,          KC_NO,          KC_NO,          LALT(LGUI(KC_UP)),LALT(LCTL(LSFT(KC_1))),KC_NO,                                 LALT(LCTL(LSFT(KC_A))),LALT(LCTL(LSFT(KC_B))),LALT(LCTL(LSFT(KC_C))),LALT(LCTL(LSFT(KC_D))),LALT(LCTL(LSFT(KC_E))),LALT(LCTL(LSFT(KC_F))),
+    LCTL(KC_SCLN),  KC_NO,          KC_NO,          LALT(LGUI(KC_DOWN)),LALT(LCTL(LSFT(KC_0))),ST_MACRO_0,                          LALT(LCTL(LSFT(KC_G))),LALT(LCTL(LSFT(KC_H))),LALT(LCTL(LSFT(KC_I))),LALT(LCTL(LSFT(KC_J))),LALT(LCTL(LSFT(KC_K))),LALT(LCTL(LSFT(KC_Q))),
+    KC_TRANSPARENT, DUAL_FUNC_3,    DUAL_FUNC_4,    DUAL_FUNC_5,    LGUI(KC_S),     LGUI(LSFT(KC_I)),                               LALT(LCTL(LSFT(KC_L))),LALT(LCTL(LSFT(KC_M))),LALT(LCTL(LSFT(KC_N))),LALT(LCTL(LSFT(KC_O))),LALT(LCTL(LSFT(KC_P))),LALT(LCTL(LSFT(KC_R))),
+                                                                    KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT
   ),
   [5] = LAYOUT_voyager(
-    NAVIGATOR_DEC_CPI,NAVIGATOR_INC_CPI,KC_TRANSPARENT, KC_MS_WH_UP,    KC_MS_WH_DOWN,  QK_LLCK,                                        QK_LLCK,        KC_MS_WH_UP,    KC_MS_WH_DOWN,  KC_TRANSPARENT, NAVIGATOR_DEC_CPI,NAVIGATOR_INC_CPI,
+    NAVIGATOR_DEC_CPI,NAVIGATOR_INC_CPI,KC_TRANSPARENT,KC_MS_WH_UP, KC_MS_WH_DOWN,  QK_LLCK,                                        QK_LLCK,        KC_MS_WH_UP,    KC_MS_WH_DOWN,  KC_TRANSPARENT, NAVIGATOR_DEC_CPI,NAVIGATOR_INC_CPI,
     KC_TRANSPARENT, KC_LEFT_SHIFT,  KC_MS_BTN5,     KC_MS_UP,       KC_MS_BTN4,     TOGGLE_SCROLL,                                  TOGGLE_SCROLL,  KC_MS_BTN5,     KC_MS_UP,       KC_MS_BTN4,     KC_LEFT_SHIFT,  KC_TRANSPARENT, 
     KC_TRANSPARENT, KC_LEFT_CTRL,   KC_MS_LEFT,     KC_MS_DOWN,     KC_MS_RIGHT,    DRAG_SCROLL,                                    DRAG_SCROLL,    KC_MS_LEFT,     KC_MS_DOWN,     KC_MS_RIGHT,    KC_LEFT_CTRL,   KC_TRANSPARENT, 
     KC_TRANSPARENT, KC_LEFT_GUI,    KC_TRANSPARENT, KC_MS_WH_LEFT,  KC_MS_WH_RIGHT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_MS_WH_LEFT,  KC_MS_WH_RIGHT, KC_TRANSPARENT, KC_LEFT_GUI,    KC_TRANSPARENT, 
-                                                    KC_MS_BTN2,     KC_MS_BTN1,                                     KC_MS_BTN1,     KC_MS_BTN2
+                                                                    KC_MS_BTN2,     KC_MS_BTN1,                                     KC_MS_BTN1,     KC_MS_BTN2
   ),
 };
 
@@ -179,11 +185,84 @@ extern bool set_scrolling;
 extern bool navigator_turbo;
 extern bool navigator_aim;
 
-
-
+bool is_app_switching = false;
+bool is_tab_switching = false;
+bool is_linux = false;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  // Unregister gui / alt / ctrl from tabbing keycodes
+  if (is_app_switching && keycode != GUI_TAB && keycode != S_GUI_TAB) {
+    unregister_code(is_linux ? KC_LALT : KC_LGUI);
+    is_app_switching = false;
+  }
+  if (is_tab_switching && keycode != CTL_TAB && keycode != S_CTL_TAB) {
+    unregister_code(KC_LCTL);
+    is_tab_switching = false;
+  }
+  
+  // Change keycodes for linux
+  if (is_linux) {
+    switch (keycode) {
+      case LGUI(KC_LEFT):
+        if (record->event.pressed) tap_code16(KC_HOME);
+        return false;
+      case LGUI(KC_RIGHT):
+        if (record->event.pressed) tap_code16(KC_END);
+        return false;
+    }
+  }
+  
   switch (keycode) {
+  case SET_MAC:
+    if (record->event.pressed) is_linux = false;
+    return false;
+  case SET_LINUX:
+    if (record->event.pressed) is_linux = true;
+    return false;
+    case GUI_TAB:
+      if (record->event.pressed) {
+        if (!is_app_switching) {
+          is_app_switching = true;
+          register_code(is_linux ? KC_LALT : KC_LGUI);
+        }
+        register_code(KC_TAB);
+        return false;
+      }
+      unregister_code(KC_TAB);
+      return false;
+    case S_GUI_TAB:
+      if (record->event.pressed) {
+        if (!is_app_switching) {
+          is_app_switching = true;
+          register_code(is_linux ? KC_LALT : KC_LGUI);
+        }
+        register_code16(S(KC_TAB));
+        return false;
+      }
+      unregister_code16(S(KC_TAB));
+      return false;
+    case CTL_TAB:
+      if (record->event.pressed) {
+        if (!is_tab_switching) {
+          is_tab_switching = true;
+          register_code(KC_LCTL);
+        }
+        register_code(KC_TAB);
+        return false;
+      }
+      unregister_code(KC_TAB);
+      return false;
+    case S_CTL_TAB:
+      if (record->event.pressed) {
+        if (!is_tab_switching) {
+          is_tab_switching = true;
+          register_code(KC_LCTL);
+        }
+        register_code16(S(KC_TAB));
+        return false;
+      }
+      unregister_code16(S(KC_TAB));
+      return false;
   case QK_MODS ... QK_MODS_MAX:
     // Mouse and consumer keys (volume, media) with modifiers work inconsistently across operating systems,
     // this makes sure that modifiers are always applied to the key that was pressed.
